53010ea23d742f9898180b5d38176921cbbaab5a
ceastman3 <connor.j.eastman@gmail.com>
2022-07-17 11:33:08 -0600

    Initial Commit Verison

---
+++ src/routeTester.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class routeTester {
+
+    // Checks to ensure that City shows corresponding data
+    // when prompted
+    @Test
+    public void cityInfoTester() {}
+
+    // Checks correct paths are returned
+    @Test
+    public void accuracyTester() {}
+
+    // Checks if code handles invalid inputs correctly
+    @Test
+    public void invalidInputTester() {}
+
+    // Checks handling of null inputs
+    @Test
+    public void nullInputTester() {}
+
+    // Checks if code properly recognizes if city has already been visited
+    // and prompts user
+    @Test
+    public void cityTrackerTester() {}
+
+}
