ec8446a05fe722f04a85bc3c9fcf8dd37014deca
ceastman3 <connor.j.eastman@gmail.com>
2022-07-18 00:41:34 -0600

    Added user interface

--- src/RouteState.java
+++ src/RouteState.java
@@ -1,2 +1,175 @@
-package PACKAGE_NAME;public enum RouteState {
+import java.util.Scanner;
+
+public enum RouteState {
+    MAIN_MENU {
+        /**
+         * Returns whether the state is DONE
+         *
+         * @return false
+         */
+        public boolean isDone() {
+            return false;
+        }
+
+        public RouteState runState(Scanner scanner, TrainSelector graph) {
+            int num;
+            System.out.println("\n[1] Learn about a city");
+            System.out.println("[2] Find the Fastest Route between Cities");
+            System.out.println("[0] Quit Program");
+            String cmd = scanner.nextLine();
+
+            try {
+                num = Integer.parseInt(cmd);
+            } catch (NumberFormatException ignored) {
+                System.out.println("Invalid command \"" + cmd + "\"");
+                return MAIN_MENU;
+            }
+
+            if (num == 1) {
+                return LEARN;
+            }
+            else if (num == 2) {
+                return FIND_ROUTE;
+            }
+            else if (num == 0) {
+                System.out.println("Ciao!");
+                return DONE;
+            }
+            else {
+                System.out.println("Warning: Invalid command \"" + cmd + "\"");
+                return MAIN_MENU;
+            }
+
+        }
+
+    },
+
+
+    DONE {
+        /**
+         * Returns whether the state is DONE
+         *
+         * @return true
+         */
+        public boolean isDone() {
+            return true;
+        }
+
+        /**
+         * Does nothing, as the application has ended
+         *
+         * @param scanner scanner for user input
+         * @return the next state of the AssignmentPlanner
+         */
+        public RouteState runState(Scanner scanner, TrainSelector graph) {
+            return DONE;
+        }
+    },
+
+
+    FIND_ROUTE {
+        /**
+         * Returns whether the state is DONE
+         *
+         * @return false
+         */
+        public boolean isDone() {
+            return false;
+        }
+
+        public RouteState runState(Scanner scanner, TrainSelector graph) {
+            boolean exists = false;
+            System.out.println("Enter starting location:");
+            String start = scanner.nextLine().toUpperCase();
+
+            // Checks name in binary search tree if exists
+
+
+
+            if (!exists) {
+                System.out.println("Invalid City: Try again");
+                // TODO: Add option to list cities (Optional)
+                return FIND_ROUTE;
+            }
+
+            System.out.println("Enter desired destination:");
+            String dest = scanner.nextLine().toUpperCase();
+
+
+
+            return DONE;
+        }
+
+
+    },
+
+
+    LEARN {
+        /**
+         * Returns whether the state is DONE
+         *
+         * @return false
+         */
+        public boolean isDone() {
+            return false;
+        }
+
+        public RouteState runState(Scanner scanner, TrainSelector graph) {
+            // TODO: Have Cities to select from
+            return DONE;
+        }
+
+
+    },
+
+    CITIES {
+        /**
+         * Returns whether the state is DONE
+         *
+         * @return false
+         */
+        public boolean isDone() {
+            return false;
+        }
+
+        public RouteState runState(Scanner scanner, TrainSelector graph) {
+            // TODO: List Cities
+            return DONE;
+        }
+
+    },
+
+    CITY_TREE {
+        /**
+         * Returns whether the state is DONE
+         *
+         * @return false
+         */
+        public boolean isDone() {
+            return false;
+        }
+        public RouteState runState(Scanner scanner, TrainSelector graph) {
+            // TODO: Find City
+            return DONE;
+        }
+    };
+
+    /**
+     * Returns whether the state is DONE
+     *
+     * @return true if this assignment is DONE
+     */
+    public abstract boolean isDone();
+
+    /**
+     * Performs the behavior of this state by utilizing the values given by the scanner, updating the
+     * queue, and returning the next state
+     *
+     * @param scanner scanner for user input
+     * @param queue   current Assignment queue
+     * @return the next state of the AssignmentPlanner
+     */
+    public abstract RouteState runState(Scanner scanner, TrainSelector graph);
+
+
 }
--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class UserInterface {
+import java.util.Scanner;
+
+public class UserInterface {
+
+    private static final String WELCOME_MSG = "Welcome to the Italian Train Route Selector";
+
+    private static final String DESCRIPTION = "Pick any two Italian cities from the list below and the program will" +
+                                                "find the shortest way to get from one city to the other via major Italian train lines";
+
+
+
+    public static void main(String[] args) {
+        System.out.println(WELCOME_MSG);
+
+        RouteState state = RouteState.MAIN_MENU;
+        TrainSelector graph = new TrainSelector();
+        Scanner scanner = new Scanner(System.in);
+
+        while (!state.isDone()) {
+            state = state.runState(scanner, graph);
+        }
+
+    }
+
 }
